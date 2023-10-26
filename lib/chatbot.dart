60da87a8d33cd0cb6afd5523c7034bb798e4c9a9
urjitupadhya <gm4175urjitupadhyay@gmail.com>
2023-10-26 17:29:43 +0530

    today6:30 25/10/2023

--- lib/chatbot.dart
+++ lib/chatbot.dart
@@ -1,247 +1,152 @@
-import 'package:login_page/feature_box.dart';
-import 'package:login_page/openai_service.dart';
-import 'package:login_page/pallete.dart';
-import 'package:animate_do/animate_do.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'package:http/http.dart' as http;
 
+void main() => runApp(MaterialApp(
+      home: Chatbot(),
+    ));
 
 class Chatbot extends StatefulWidget {
-  const Chatbot({super.key});
-
   @override
-  State<Chatbot> createState() => ChatbotState();
+  _ChatbotState createState() => _ChatbotState();
 }
 
-class ChatbotState extends State<Chatbot> {
-  final speechToText = SpeechToText();
-  final flutterTts = FlutterTts();
-  String lastWords = '';
-  final OpenAIService openAIService = OpenAIService();
-  String? generatedContent;
-  String? generatedImageUrl;
-  int start = 200;
-  int delay = 200;
+class _ChatbotState extends State<Chatbot> {
+  final List<Message> _messages = [];
+  final TextEditingController _textEditingController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    initSpeechToText();
-    initTextToSpeech();
-  }
+  void onSendMessage() async {
+    String userMessage = _textEditingController.text;
+    _textEditingController.clear();
 
-  Future<void> initTextToSpeech() async {
-    await flutterTts.setSharedInstance(true);
-    setState(() {});
-  }
-
-  Future<void> initSpeechToText() async {
-    await speechToText.initialize();
-    setState(() {});
-  }
-
-  Future<void> startListening() async {
-    await speechToText.listen(onResult: onSpeechResult);
-    setState(() {});
-  }
-
-  Future<void> stopListening() async {
-    await speechToText.stop();
-    setState(() {});
-  }
-
-  void onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
-      lastWords = result.recognizedWords;
+      _messages.add(Message(text: userMessage, isUser: true));
+    });
+
+    // Show a "generating" message
+    setState(() {
+      _messages.add(Message(text: "Generating...", isUser: false));
+    });
+
+    String response = await sendMessageToChatGpt(userMessage);
+
+    // Remove the "generating" message and show the actual response
+    _messages.removeWhere((message) => message.text == "Generating...");
+    setState(() {
+      _messages.add(Message(text: response, isUser: false));
     });
   }
 
-  Future<void> systemSpeak(String content) async {
-    await flutterTts.speak(content);
+  Future<String> sendMessageToChatGpt(String message) async {
+    Uri uri = Uri.parse("https://api.openai.com/v1/chat/completions");
+
+    Map<String, dynamic> body = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": message}
+      ],
+      "max_tokens": 500,
+    };
+
+    final response = await http.post(
+      uri,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 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",
+      },
+      body: json.encode(body),
+    );
+
+    Map<String, dynamic> parsedResponse = json.decode(response.body);
+    String reply = parsedResponse['choices'][0]['message']['content'];
+
+    return reply;
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    speechToText.stop();
-    flutterTts.stop();
+  Widget _buildMessage(Message message) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Column(
+          crossAxisAlignment:
+              message.isUser ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: message.isUser ? Colors.grey[300] : Colors.blue[100],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text(
+                message.text,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: BounceInDown(
-          child: const Text('FINDERBOT'),
-        ),
-        leading: const Icon(Icons.menu),
-        centerTitle: true,
+        title: Text('Chatbot'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // virtual assistant picture
-            ZoomIn(
-              child: Stack(
-                children: [
-                  Center(
-                    child: Container(
-                      height: 120,
-                      width: 120,
-                      margin: const EdgeInsets.only(top: 4),
-                      decoration: const BoxDecoration(
-                        color: Pallete.assistantCircleColor,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 123,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/images/virtualAssistant.png',
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'assets/images/ic.gif',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _buildMessage(_messages[index]);
+                  },
+                ),
+              ),
+              Divider(height: 1.0),
+              Container(
+                decoration: BoxDecoration(color: Theme.of(context).cardColor),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: _textEditingController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(10.0),
+                          hintText: 'Type a message...',
+                          border: InputBorder.none,
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            // chat bubble
-            FadeInRight(
-              child: Visibility(
-                visible: generatedImageUrl == null,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                  margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-                    top: 30,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Pallete.borderColor,
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: onSendMessage,
                     ),
-                    borderRadius: BorderRadius.circular(20).copyWith(
-                      topLeft: Radius.zero,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text(
-                      generatedContent == null
-                          ? 'Good Morning, what task can I do for you?'
-                          : generatedContent!,
-                      style: TextStyle(
-                        fontFamily: 'Cera Pro',
-                        color: Pallete.mainFontColor,
-                        fontSize: generatedContent == null ? 25 : 18,
-                      ),
-                    ),
-                  ),
+                  ],
                 ),
               ),
-            ),
-            if (generatedImageUrl != null)
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(generatedImageUrl!),
-                ),
-              ),
-            SlideInLeft(
-              child: Visibility(
-                visible: generatedContent == null && generatedImageUrl == null,
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(top: 10, left: 22),
-                  child: const Text(
-                    'Here are a few features',
-                    style: TextStyle(
-                      fontFamily: 'Cera Pro',
-                      color: Pallete.mainFontColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            // features list
-            Visibility(
-              visible: generatedContent == null && generatedImageUrl == null,
-              child: Column(
-                children: [
-                  SlideInLeft(
-                    delay: Duration(milliseconds: start),
-                    child: const FeatureBox(
-                      color: Pallete.firstSuggestionBoxColor,
-                      headerText: 'ChatGPT',
-                      descriptionText:
-                          'A smarter way to stay organized and informed with ChatGPT',
-                    ),
-                  ),
-                  SlideInLeft(
-                    delay: Duration(milliseconds: start + delay),
-                    child: const FeatureBox(
-                      color: Pallete.secondSuggestionBoxColor,
-                      headerText: 'Dall-E',
-                      descriptionText:
-                          'Get inspired and stay creative with your personal assistant powered by Dall-E',
-                    ),
-                  ),
-                  SlideInLeft(
-                    delay: Duration(milliseconds: start + 2 * delay),
-                    child: const FeatureBox(
-                      color: Pallete.thirdSuggestionBoxColor,
-                      headerText: 'Smart Voice Assistant',
-                      descriptionText:
-                          'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: ZoomIn(
-        delay: Duration(milliseconds: start + 3 * delay),
-        child: FloatingActionButton(
-          backgroundColor: Pallete.firstSuggestionBoxColor,
-          onPressed: () async {
-            if (await speechToText.hasPermission &&
-                speechToText.isNotListening) {
-              await startListening();
-            } else if (speechToText.isListening) {
-              final speech = await openAIService.isArtPromptAPI(lastWords);
-              if (speech.contains('https')) {
-                generatedImageUrl = speech;
-                generatedContent = null;
-                setState(() {});
-              } else {
-                generatedImageUrl = null;
-                generatedContent = speech;
-                setState(() {});
-                await systemSpeak(speech);
-              }
-              await stopListening();
-            } else {
-              initSpeechToText();
-            }
-          },
-          child: Icon(
-            speechToText.isListening ? Icons.stop : Icons.mic,
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
+}
+
+class Message {
+  final String text;
+  final bool isUser;
+
+  Message({required this.text, required this.isUser});
 }
